9b7ee99e80ed7ff20eacb828c3d696d7ea2a21a2
為什麼 <andy840119@gmail.com>
2018-09-03 10:29:53 +0800

    實作完成

--- FcbktwtpExchangeRateProvider.cs
+++ FcbktwtpExchangeRateProvider.cs
@@ -4,8 +4,10 @@ using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Xml;
+using HtmlAgilityPack;
 using Nop.Core;
 using Nop.Core.Plugins;
+using Nop.Plugin.ExchangeRate.FcbktwtpExchange;
 using Nop.Services.Directory;
 using Nop.Services.Localization;
 using Nop.Services.Logging;
@@ -46,67 +48,100 @@ namespace Nop.Plugin.ExchangeRate.EcbExchange
                 throw new ArgumentNullException(nameof(exchangeRateCurrencyCode));
 
             //add euro with rate 1
-            var ratesToEuro = new List<Core.Domain.Directory.ExchangeRate>
+            var ratesToTwd = new List<Core.Domain.Directory.ExchangeRate>
             {
                 new Core.Domain.Directory.ExchangeRate
                 {
-                    CurrencyCode = "EUR",
+                    CurrencyCode = "TWD",
                     Rate = 1,
                     UpdatedOn = DateTime.UtcNow
                 }
             };
 
-            //get exchange rates to euro from European Central Bank
-            var request = (HttpWebRequest)WebRequest.Create("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
-            try
+            var currencyList = new HtmlWeb().Load("https://ibank.firstbank.com.tw/NetBank/7/0201.html?sh=none")
+                .DocumentNode.SelectNodes("//table[@id='table1']//tr");
+            var rateResult = new List<FcbktwtpRateObject>();
+            //第一銀行的第1個tr是title,略過不做。
+            for (int i = 1; i < currencyList.Count; i++)
             {
-                using (var response = request.GetResponse())
+                string cashBuying = "-";
+                string cashSelling = "-";
+                string spotBuying = "-";
+                string spotSelling = "-";
+
+                //小發現，這裡的編排是即期先，再來才是現金。
+                //這是匯率一定會有即期，現金則不一定。
+                string currency = currencyList[i].SelectSingleNode("td[1]").InnerText.Trim().Replace("&nbsp;", String.Empty);
+                string nextCurrency;
+                if (i + 1 == currencyList.Count)
                 {
-                    //load XML document
-                    var document = new XmlDocument();
-                    document.Load(response.GetResponseStream());
-
-                    //add namespaces
-                    var namespaces = new XmlNamespaceManager(document.NameTable);
-                    namespaces.AddNamespace("ns", "http://www.ecb.int/vocabulary/2002-08-01/eurofxref");
-                    namespaces.AddNamespace("gesmes", "http://www.gesmes.org/xml/2002-08-01");
-
-                    //get daily rates
-                    var dailyRates = document.SelectSingleNode("gesmes:Envelope/ns:Cube/ns:Cube", namespaces);
-                    if (!DateTime.TryParseExact(dailyRates.Attributes["time"].Value, "yyyy-MM-dd", null, DateTimeStyles.None, out DateTime updateDate))
-                        updateDate = DateTime.UtcNow;
-
-                    foreach (XmlNode currency in dailyRates.ChildNodes)
-                    {
-                        //get rate
-                        if (!decimal.TryParse(currency.Attributes["rate"].Value, out decimal currencyRate))
-                            continue;
-
-                        ratesToEuro.Add(new Core.Domain.Directory.ExchangeRate()
-                        {
-                            CurrencyCode = currency.Attributes["currency"].Value,
-                            Rate = currencyRate,
-                            UpdatedOn = updateDate
-                        });
-                    }
+                    nextCurrency = null;
                 }
+                else
+                {
+                    nextCurrency = currencyList[i + 1].SelectSingleNode("td[1]").InnerText.Trim().Replace("&nbsp;", String.Empty); //下一個tr的幣種
+                }
+                
+                //如果下一行也是該幣種，代表有現金和即期匯率。
+                //沒有的話只有即期匯率。
+                if (currency == nextCurrency)
+                {
+                    spotBuying = currencyList[i].SelectSingleNode("td[3]").InnerText.Trim();
+                    spotSelling = currencyList[i].SelectSingleNode("td[4]").InnerText.Trim();
+                    cashBuying = currencyList[i+1].SelectSingleNode("td[3]").InnerText.Trim();
+                    cashSelling = currencyList[i+1].SelectSingleNode("td[4]").InnerText.Trim();
+                    i++;
+                }
+                else
+                {
+                    spotBuying = currencyList[i].SelectSingleNode("td[3]").InnerText.Trim();
+                    spotSelling = currencyList[i].SelectSingleNode("td[4]").InnerText.Trim();
+                }
+
+                string[] splitCurrencyString = null;
+                splitCurrencyString = currency.Split("(");
+                currency = splitCurrencyString[0];
+                string currencyCode;
+                if (splitCurrencyString.Length == 1) currencyCode = currency;
+                else currencyCode= splitCurrencyString[1].Replace(")", String.Empty);
+
+                rateResult.Add(new FcbktwtpRateObject
+                {
+                    Currency = currency,
+                    CurrencyCode = currencyCode,
+                    CashBuying = (!cashBuying.Contains("-")) ? Convert.ToDecimal(cashBuying) : new decimal?(),
+                    CashSelling = (!cashSelling.Contains("-")) ? Convert.ToDecimal(cashSelling) : new decimal?(),
+                    SpotBuying = (!spotBuying.Contains("-")) ? Convert.ToDecimal(spotBuying) : new decimal?(),
+                    SpotSelling = (!spotSelling.Contains("-")) ? Convert.ToDecimal(spotSelling) : new decimal?()
+                });
             }
-            catch (WebException ex)
-            {
-                _logger.Error("ECB exchange rate provider", ex);
+
+            //converr rate to nop currency rate object
+            foreach(var rate in rateResult)
+            { 
+                var averageRate = rate.SpotBuying!=null
+                    ? (1 / (( rate.SpotBuying.Value + rate.SpotSelling.Value ) / 2))
+                    : (1 / (( rate.CashBuying.Value + rate.CashBuying.Value ) / 2));
+
+                ratesToTwd.Add(new Core.Domain.Directory.ExchangeRate
+                { 
+                    CurrencyCode = rate.CurrencyCode,
+                    Rate = averageRate,
+                    UpdatedOn = DateTime.UtcNow,
+                });
             }
 
             //return result for the euro
-            if (exchangeRateCurrencyCode.Equals("eur", StringComparison.InvariantCultureIgnoreCase))
-                return ratesToEuro;
+            if (exchangeRateCurrencyCode.Equals("twd", StringComparison.InvariantCultureIgnoreCase))
+                return ratesToTwd;
 
-            //use only currencies that are supported by ECB
-            var exchangeRateCurrency = ratesToEuro.FirstOrDefault(rate => rate.CurrencyCode.Equals(exchangeRateCurrencyCode, StringComparison.InvariantCultureIgnoreCase));
+            //use only currencies that are supported by FCBKTWTP
+            var exchangeRateCurrency = ratesToTwd.FirstOrDefault(rate => rate.CurrencyCode.Equals(exchangeRateCurrencyCode, StringComparison.InvariantCultureIgnoreCase));
             if (exchangeRateCurrency == null)
-                throw new NopException(_localizationService.GetResource("Plugins.ExchangeRate.EcbExchange.Error"));
+                throw new NopException(_localizationService.GetResource("Plugins.ExchangeRate.FcbktwtpExchange.Error"));
 
             //return result for the selected (not euro) currency
-            return ratesToEuro.Select(rate => new Core.Domain.Directory.ExchangeRate
+            return ratesToTwd.Select(rate => new Core.Domain.Directory.ExchangeRate
             {
                 CurrencyCode = rate.CurrencyCode,
                 Rate = Math.Round(rate.Rate / exchangeRateCurrency.Rate, 4),
@@ -120,7 +155,7 @@ namespace Nop.Plugin.ExchangeRate.EcbExchange
         public override void Install()
         {
             //locales
-            _localizationService.AddOrUpdatePluginLocaleResource("Plugins.ExchangeRate.EcbExchange.Error", "You can use ECB (European central bank) exchange rate provider only when the primary exchange rate currency is supported by ECB");
+            _localizationService.AddOrUpdatePluginLocaleResource("Plugins.ExchangeRate.FcbktwtpExchange.Error", "You can use FCBKTWTP (European central bank) exchange rate provider only when the primary exchange rate currency is supported by FCBKTWTP");
 
             base.Install();
         }
@@ -131,7 +166,7 @@ namespace Nop.Plugin.ExchangeRate.EcbExchange
         public override void Uninstall()
         {
             //locales
-            _localizationService.DeletePluginLocaleResource("Plugins.ExchangeRate.EcbExchange.Error");
+            _localizationService.DeletePluginLocaleResource("Plugins.ExchangeRate.FcbktwtpExchange.Error");
 
             base.Uninstall();
         }
---
+++ FcbktwtpRateObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Plugin.ExchangeRate.FcbktwtpExchange
+{
+    public class FcbktwtpRateObject
+    {
+        //匯率國家(中文)
+        public string Currency { get; set; }
+
+        //匯率代碼
+        public string CurrencyCode { get; set; }
+
+        //現今買入價
+        public decimal? CashBuying { get; set; }
+
+        //現金賣出價
+        public decimal? CashSelling { get; set; }
+
+        //及時買入價
+        public decimal? SpotBuying { get; set; }
+
+        //及時賣出價
+        public decimal? SpotSelling { get; set; }
+    }
+}
